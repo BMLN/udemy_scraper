9c976ff242b5c8312d122d0d224a1aef41c2cff6
BMLN <lorandbanki@gmail.com>
2022-08-09 01:02:02 +0200

    close #1

--- src/udemy_scraper.py
+++ src/udemy_scraper.py
@@ -1,18 +1,17 @@
 import os
-import warnings
 import ex_questions
-import argparse
+from argparse import ArgumentParser
 from lxml import etree
 
 
-#CONSTS
+#"CONSTS"
 WEBSRCS = [ str(y[0]) + "/" + str(x) for y in os.walk("../websource") for x in y[2] ]
 OUTPUT_PATH = "../output"
 
 
 
 
-
+#DEFS
 class Question:
     """ wrapper class for the question contents """
 
@@ -22,7 +21,7 @@ class Question:
         self.answers = answers
 
     def __str__(self):
-        return ",".join([ str(self.number), self.question, str(self.answers) ])
+        return ",".join([ str(self.number), str(self.question), str(self.answers) ])
 
 
 
@@ -36,32 +35,31 @@ def parse_webpage(path_to_webpage):
         returns:
             a list of questions
     """
-    webpage = ""
+    webpage = "default"
     questions = []
 
     try:
         file = open(path_to_webpage, "r")
         webpage = file.read()
         file.close()
-        webpage = etree.HTML(webpage)
-        webpage = webpage.xpath("""//div[contains(@class, "detailed-result-panel--question-container")]""")
 
     except FileNotFoundError:
-        warnings.warn("No file for " + path_to_webpage)
+        print("Warning:", "No file for", path_to_webpage)
 
+    webpage = etree.HTML(webpage)
+    webpage = webpage.xpath("""//div[contains(@class, "detailed-result-panel--question-container")]""")
 
-    if len(webpage) > 0:
-        for question in webpage:
+    for question in webpage:
 
-            __num = question.xpath(""".//span""")
-            __question = question.xpath(""".//div[contains(@id, "question-prompt")]""")
-            __answers = question.xpath(""".//div[contains(@class, "mc-quiz-answer--answer-inner")]""")
+        __num = question.xpath(""".//span""")
+        __question = question.xpath(""".//div[contains(@id, "question-prompt")]""")
+        __answers = question.xpath(""".//div[contains(@class, "mc-quiz-answer--answer-inner")]""")
 
-            for x in range(len(__answers)):
-                __answers[x] = ( __answers[x][0].text, len(__answers[x]) == 2)
+        for x in range(len(__answers)):
+            __answers[x] = ( __answers[x][0].text, len(__answers[x]) == 2)
 
-            if min([len(__num), len(__question), len(__answers)]) > 0:
-                questions.append(Question(__num[0].text, __question[0].text, __answers))
+        if min([len(__num), len(__question), len(__answers)]) > 0:
+            questions.append(Question(__num[0].text, __question[0].text, __answers))
 
     return questions
 
@@ -81,25 +79,26 @@ def filter_questions(questions):
 
 
 
+#PROG
 if __name__ == "__main__":
     """ writes every question from ./websource into files """
     count_doubled = 0
     
-    arg_parser = argparse.ArgumentParser(description="writes questions from udemy websources into files")
-    arg_parser.add_argument("--src", type=list, help="all the source files", default=WEBSRCS)
+    arg_parser = ArgumentParser(description="writes questions from udemy websources into files")
+    arg_parser.add_argument("--src", type=str, nargs="+", help="all the source files", default=WEBSRCS)
     arg_parser.add_argument("--dest", type=str, help="the destination folder for the generated files", default=OUTPUT_PATH)
     args = arg_parser.parse_args()
 
-    args.src = list(filter(lambda file_name: file_name.endswith(".html"), args.src))
 
     #x = 0
-    for src_file in args.src:
-        par_dir = src_file.split("/")[-2]
-        file_name = src_file.split("/")[-1]
-        os.makedirs(OUTPUT_PATH + "/" + par_dir, exist_ok=True)
+    for src_file in list(filter(lambda file_name: file_name.endswith(".html"), args.src)):
+        
+        par_dir = src_file.split("/")[-2] if "/" in src_file else "./"
+        file_name = src_file.split("/")[-1] if "/" in src_file else src_file
+        os.makedirs(os.path.join(OUTPUT_PATH, par_dir), exist_ok=True)
 
         print("Converting: ", file_name)
-
+        
         questions = parse_webpage(src_file)
         questions_filtered = filter_questions(questions)
         count_doubled += len(questions) - len(questions_filtered)
